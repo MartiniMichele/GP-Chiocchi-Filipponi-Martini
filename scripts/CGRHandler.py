bc43b43b02ea0159f7a2aec28561519ff57eb118
MartiniMichele <martini.michele99@gmail.com>
2023-01-24 12:45:36 +0100

    dataset commit
    committed all source and output dataset

--- scripts/CGRHandler.py
+++ scripts/CGRHandler.py
@@ -38,33 +38,60 @@ class CGRHandler:
 
                 # call read text file function
                 read_text_file(file_path)
+                print(file_path)
                 self.generate_dataset(counter)
                 counter += 1
 
     def filter_sequence(self, sequence):
 
         filtered_sequence = MutableSeq(str(sequence[0]))
-        bio_sequence = MutableSeq(str(sequence[0]))
+        chars = ["Y", "N", "R", "M", "S", "W", "K"]
 
-        if bio_sequence.find("Y") != -1:
-            filtered_sequence = bio_sequence.replace("Y", "")
+        if any(x in sequence[0] for x in chars):
 
-        elif bio_sequence.find("N") != -1:
-            filtered_sequence = bio_sequence.replace("N", "")
+            char_count = self.count_char(sequence)
 
-        elif bio_sequence.find("R") != -1:
-            filtered_sequence = bio_sequence.replace("R", "")
+            for i in range(0, char_count):
 
-        elif bio_sequence.find("M") != -1:
-            filtered_sequence = bio_sequence.replace("M", "")
+                if filtered_sequence.find("Y") != -1:
+                    filtered_sequence = filtered_sequence.replace("Y", "")
 
-        elif bio_sequence.find("S") != -1:
-            filtered_sequence = bio_sequence.replace("S", "")
+                elif filtered_sequence.find("N") != -1:
+                    filtered_sequence = filtered_sequence.replace("N", "")
 
-        else:
-            pass
+                elif filtered_sequence.find("R") != -1:
+                    filtered_sequence = filtered_sequence.replace("R", "")
+
+                elif filtered_sequence.find("M") != -1:
+                    filtered_sequence = filtered_sequence.replace("M", "")
+
+                elif filtered_sequence.find("S") != -1:
+                    filtered_sequence = filtered_sequence.replace("S", "")
+
+                elif filtered_sequence.find("W") != -1:
+                    filtered_sequence = filtered_sequence.replace("W", "")
+
+                elif filtered_sequence.find("K") != -1:
+                    filtered_sequence = filtered_sequence.replace("K", "")
+
+                else:
+                    pass
 
         return filtered_sequence
+
+    def count_char(self, sequence):
+        tmp_sequence = MutableSeq(sequence[0])
+        char_count = 0
+
+        char_count += tmp_sequence.count("Y")
+        char_count += tmp_sequence.count("N")
+        char_count += tmp_sequence.count("R")
+        char_count += tmp_sequence.count("M")
+        char_count += tmp_sequence.count("S")
+        char_count += tmp_sequence.count("W")
+        char_count += tmp_sequence.count("K")
+
+        return char_count
 
     def generate_dataset(self, counter):
 
