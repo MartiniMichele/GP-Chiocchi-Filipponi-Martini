f19ec6f116516861a015691687e3cf6b96e21fb4
FrancescoChiocchi8 <francesco.chiocchi@studenti.unicam.it>
2023-02-01 14:35:22 +0100

    Eliminazione molecole duplicate o con lunghezza <100 || >130

---
+++ scripts/DeleteDuplicates.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Caricamento del file 5s.xlsx
+df = pd.read_excel('C:/Users/fchio/Desktop/GroupProject/GP-Chiocchi-Filipponi-Martini/Fasta_5S/Origin_File/5s.xlsx')
+
+# Rimozione dei duplicati nella colonna Organism name
+df.drop_duplicates(subset='Organism name ', keep='first', inplace=True)
+
+# Elimino i valori nulli della colonna Organism name
+df.dropna(subset=['Organism name '], inplace=True)
+
+# Rimuovo le righe con lunghezza inferiore a 100 e maggiore di 130
+df = df[(df['Length'] >= 100) & (df['Length'] <= 130)]
+
+# Salvataggio del risultato in un nuovo file xlsx nella cartella results
+df.to_excel('C:/Users/fchio/Desktop/GroupProject/GP-Chiocchi-Filipponi-Martini/Fasta_5S/Results/result.xlsx', index=False)
